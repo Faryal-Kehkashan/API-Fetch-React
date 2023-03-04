8e305869ad1fd1d61005bd54457c98821d541b19
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-03-04 11:34:47 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [state, setState] = useState();
+  const [prog, setProg] = useState();
+
+  //API fetching through post mehtod
+
+  const handlesubmit = () => {
+    axios
+      .post("http://localhost:5000/data", { name: state, program: prog })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  //API fetching through delete mehtod
+
+  const handledelete = () => {
+    axios.delete("http://localhost:5000/data/3").then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  //API fetching through update method
+
+  const handleUpdate = () => {
+    axios
+      .put("http://localhost:5000/data/1", { name: "Faryal Kehkashan" })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  //API fetching through get mehtod using useEffect Hook
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/data")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Name</label>
+      <input
+        value={state}
+        onChange={(e) => {
+          setState(e.target.value);
+        }}
+      />
+      <label>Program</label>
+      <input
+        value={prog}
+        onChange={(e) => {
+          setProg(e.target.value);
+        }}
+      />
+      <button onClick={handlesubmit}>Submit</button>
+      <button onClick={handledelete}>Delete</button>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
